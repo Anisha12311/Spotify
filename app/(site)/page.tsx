3b518b8d1522bf226f46bb51e550566bfb40aa0f
Anisha Yadav <anishay@quantorix.com>
2023-11-16 20:35:45 +0530

    Media player

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 import PageContent from "./component/PageContent";
 
-export const revaildate = 0;
+export const revalidate = 0;
 export default async function Home() {
   const songs = await getSongs();
   return (
